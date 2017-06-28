3b0f857f4bd064559e1525439832c1b02da08b15
berangere.la.touche <berangere.lt@gmail.com>
2017-06-28 21:48:40 +0200

    toolbar functions in progress, geolocalisation ok

--- GeoSkate/app/src/main/java/com/berangerelatouche/geoskate/HomeActivity.java
+++ GeoSkate/app/src/main/java/com/berangerelatouche/geoskate/HomeActivity.java
@@ -1,16 +1,23 @@
 package com.berangerelatouche.geoskate;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -20,8 +27,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public GoogleMap mMap;
+    public UiSettings uiSettings;
     public Mylocation mylocation;
+    public Marker marker;
+    public MarkerOptions markerOptions;
     public Location location;
+    public ImageButton add;
+    public ImageButton search;
+    public ImageButton settings;
     double lat, lng;
 
     @Override
@@ -32,6 +45,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#243744"));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
+        add = (ImageButton) findViewById(R.id.addButton);
+        search = (ImageButton) findViewById(R.id.searchButton);
+        settings = (ImageButton) findViewById(R.id.settingsButton);
 
         mylocation = new Mylocation(getApplicationContext());
         location = mylocation.getLocation();
@@ -50,12 +67,28 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        uiSettings = mMap.getUiSettings();
 
-        mMap.isMyLocationEnabled();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setCompassEnabled(true);
 
         LatLng mylocation = new LatLng(lat, lng);
-        mMap.addMarker(new MarkerOptions().position(mylocation).title("You are here"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
+        mMap.addMarker(new MarkerOptions().position(mylocation).draggable(true).title("You are here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mylocation, 13));
+
+        toolBarFunctions();
+    }
+
+    public void toolBarFunctions() {
+
+        add.ad
+
     }
 
 }
