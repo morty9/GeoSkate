3b1a89dfcdc8d909cac62f91df457eca7406ee75
berangere.la.touche <berangere.lt@gmail.com>
2017-06-28 23:28:42 +0200

    get marker location in progress

--- GeoSkate/app/src/main/java/com/berangerelatouche/geoskate/Toolbar_Fragment.java
+++ GeoSkate/app/src/main/java/com/berangerelatouche/geoskate/Toolbar_Fragment.java
@@ -1,11 +1,16 @@
 package com.berangerelatouche.geoskate;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by berangerelatouche on 25/06/2017.
@@ -13,10 +18,34 @@ import android.view.ViewGroup;
 
 public class Toolbar_Fragment extends Fragment {
 
+    public ImageButton add;
+    public ImageButton search;
+    public ImageButton settings;
+    public HomeActivity homeData;
+    public LatLng adding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.toolbar_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.toolbar_fragment, container, false);
+
+        add = view.findViewById(R.id.addButton);
+
+        //toolBarFunctions();
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //toolBarFunctions();
     }
 
     public static Toolbar_Fragment newInstance() {
@@ -27,4 +56,24 @@ public class Toolbar_Fragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    /*public void toolBarFunctions(View view) {
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("TEST BUTTON");
+                if (homeData.marker.getPosition() != null) {
+                    double latitude = homeData.marker.getPosition().latitude;
+                    double longitude = homeData.marker.getPosition().longitude;
+                    adding = new LatLng(latitude, longitude);
+                } else {
+                    System.out.println("Error");
+                }
+
+            }
+        });
+
+        System.out.println("ADDING LOCATION" + adding);
+    }*/
 }
